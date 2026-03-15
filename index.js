5363db9a94e115c39e007f346a4283ab7b631764
SunilKanzariya249 <kanzariyasunil249@gmail.com>
2026-03-15 21:59:26 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const Blog=require("./models/blog")
+const app = express();
+const userRoutes = require("./routes/user");
+const blogRoutes = require("./routes/blog");
+const { default: mongoose } = require("mongoose");
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+app.use(express.static(path.resolve("./public")))
+const PORT = 8000;
+mongoose.connect("mongodb://localhost:27017/blogify").then(() => {
+  console.log("mongodb connected");
+});
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.get("/", async(req, res) => {
+  const blogs=await Blog.find({})
+  return res.render("home", {
+   blogs,
+  });
+});
+app.use("/user", userRoutes);
+app.use("/blog", blogRoutes);
+app.listen(PORT, () => {
+  console.log(`app run on port${PORT}`);
+});
