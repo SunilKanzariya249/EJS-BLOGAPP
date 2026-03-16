9d571b9f1f3e72dc6e52b32a4e2a3847aaa8cf21
SunilKanzariya249 <kanzariyasunil249@gmail.com>
2026-03-16 14:15:41 +0530

    Css update

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,9 @@ const User = require("../models/user");
 const router = Router();
 
 router.get("/signin", (req, res) => {
-  return res.render("signin");
+  return res.render("signin",{
+   
+  });
 });
 
 router.get("/signup", (req, res) => {
